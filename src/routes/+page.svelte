8935a79f6d9008651cf461a8701cb5c1663a2976
jovany velez <jovany.velez@udea.edu.co>
2024-03-19 12:31:49 -0500

    inicio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<main class="flex flex-col">
+
+
+    <h1 class="text-4xl font-bold text-center my-10">INFORMACIÓN IMPORTANTE!!!</h1>
+    
+    <div>
+        <h2 class="p-10 text-2xl font-semibold">Atn: Señores Propietarios Edificio Mirador de Altavista.</h2>
+    </div>
+
+    <p class="w-11/12  mx-auto my-5">
+        <span class="font-bold">La asamblea de propietarios del Edificio Mirador de Altavista se realizará el  lunes 01 de abril en las instalaciones del edificio a las 8:00 de la noche </span>. Esta reunión corresponde a una reunión por derecho propio tal cual lo establece el artículo 40 de la ley 675 de 2001 (Régimen de propiedad horizontal).
+    </p>
+
+    <p class="w-11/12  mx-auto">
+        <span class="font-bold">Lo anterior debido a que no se llamó a la Asamblea Ordinaria de Propietarios para este año</span>, tal cual lo expresa la LEY 675 DE 2001 en su artículo 39.
+    </p>
+
+    <p class="w-11/12 mx-auto my-5">
+        Frente a cualquier duda se anexa la información expresada por la Ley:
+    </p>
+
+    <a class="w-11/12 mx-auto text-blue-600" href="http://www.secretariasenado.gov.co/senado/basedoc/ley_0675_2001_pr001.html#39" target="_blank" rel="noopener">Haz click aquí para ver el articulo <span class="text-red-600">39</span> de la ley 675</a>
+    <a class="w-11/12 mx-auto text-blue-600" href="http://www.secretariasenado.gov.co/senado/basedoc/ley_0675_2001_pr001.html#39" target="_blank" rel="noopener">Haz click aquí para ver el articulo <span class="text-red-600">40</span> de la ley 675</a>
+
+</main>
