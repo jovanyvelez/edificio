ae4d33d3b1fde9019e78f86c0d48ea1b1e3e1c7b
jovany <jvelez23@misena.edu.co>
2025-03-11 06:17:18 -0500

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+<h1 class="text-center text-3xl my-5 font-bold">Edificio Mirador de Altavista PH</h1>
+{@render children()}
