3a15e9230e005eb9fffa63c8b087beef4536eb68
jovany <jvelez23@misena.edu.co>
2025-03-16 11:03:22 -0500

    correccion

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,4 +1,4 @@
-import { DATABASE_URL, /*DATABASE_TOKEN */} from '$env/static/private';
+import { DATABASE_URL, DATABASE_TOKEN } from '$env/static/private';
 import { dev } from '$app/environment';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
@@ -6,9 +6,9 @@ import * as schema from '$lib/server/db/data';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL no fue definida en entorno');
 
-/*if (!dev && !DATABASE_TOKEN)
-	throw new Error('DATABASE_AUTH_TOKEN no fue definida en entorno');*/
+if (!dev && !DATABASE_TOKEN)
+	throw new Error('DATABASE_AUTH_TOKEN no fue definida en entorno');
 
-const client = createClient({ url: DATABASE_URL,/* authToken: DATABASE_TOKEN */});
+const client = createClient({ url: DATABASE_URL, authToken: DATABASE_TOKEN });
 
 export const db = drizzle(client, {schema ,casing: 'snake_case' });
--- src/routes/presupuesto/+page.server.ts
+++ src/routes/presupuesto/+page.server.ts
@@ -1,8 +1,8 @@
-import { getFullPresupuesto } from "$lib/server/db/orm/queries/presupuesto";
+/*import { getFullPresupuesto } from "$lib/server/db/orm/queries/presupuesto";
 
 export const load = async () => {
     const presupuesto = await getFullPresupuesto(2025);
     return {
         datos: presupuesto
     };
-}
+}*/
