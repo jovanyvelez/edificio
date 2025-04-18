e8c5a0feb9a13ae90aa23ba5d0d46cf7ddf32a07
jovany <jvelez23@misena.edu.co>
2025-04-18 16:41:09 -0500

    env conf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'drizzle-kit';
 
 if (!process.env.DATABASE_URL) throw new globalThis.Error('DATABASE_URL is not set');
 
-if (!process.env.DATABASE_TOKEN) throw new globalThis.Error('DATABASE_TOKEN is not set');
-
 export default defineConfig({
   out: './drizzle',
   schema:'./src/lib/server/db/data.ts',
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,14 +1,15 @@
-import { DATABASE_URL, DATABASE_TOKEN } from '$env/static/private';
-import { dev } from '$app/environment';
+//import { DATABASE_URL, DATABASE_TOKEN } from '$env/static/private';
+import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from '$lib/server/db/data';
 
-if (!DATABASE_URL) throw new Error('DATABASE_URL no fue definida en entorno');
 
-if (!dev && !DATABASE_TOKEN)
-	throw new Error('DATABASE_AUTH_TOKEN no fue definida en entorno');
 
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_TOKEN });
+if (!process.env.DATABASE_URL) throw new globalThis.Error('DATABASE_URL is not set');
+
+if (!process.env.DATABASE_TOKEN) throw new globalThis.Error('DATABASE_TOKEN is not set');
+
+const client = createClient({ url: process.env.DATABASE_URL, authToken: process.env.DATABASE_TOKEN });
 
 export const db = drizzle(client, {schema ,casing: 'snake_case' });
